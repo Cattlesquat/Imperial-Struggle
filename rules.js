4865a9d9cdb4302cf7de36a3585401b224cd1d94
Brian Reynolds <brian@brianreynolds.net>
2026-01-25 19:06:41 -0500

    Remove debug code

--- rules.js
+++ rules.js
@@ -7919,16 +7919,7 @@ function is_prussia(s)
 
 function action_point_cost (who, s, type, ignore_region_switching = false)
 {
-	debug_log ("I am here!" + s)
-	on_view(R)
-	if (s < 0 || (s >= NUM_SPACES)) {
-		debug_log ("Wrong space somehow: " + s)
-		on_view(R)
-		return 0
-	}
 	var cost = data.spaces[s].cost
-
-	return 0
 
 	// These two "side effect" fields allow us to explain complex cost adjustments to the player so we don't get unending "bug" reports
 	G.action_cost_breakdown = "Space cost: " + cost + "."
