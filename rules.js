c8fccb341f11d56fd65b532aef9e4e36eb1e6fba
Brian Reynolds <brian@brianreynolds.net>
2026-02-06 14:46:58 -0500

    Prevent a reference error

--- rules.js
+++ rules.js
@@ -3894,7 +3894,7 @@ P.select_investment_tile = {
 		log_box_end()
 	},
 	inactive() {
-		if (G.theater_bonus_war_tiles[1-R][0].length) {
+		if (G.theater_bonus_war_tiles && G.theater_bonus_war_tiles[1-R][0] && G.theater_bonus_war_tiles[1-R][0].length) {
 			return "place Austrian Succession bonus war tiles"
 		} else {
 			return "select an investment tile"
