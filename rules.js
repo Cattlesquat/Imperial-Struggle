0e926afedfa0f23b1205d106304d4c2f7e6a7324
Brian Reynolds <brian@brianreynolds.net>
2026-02-11 14:49:00 -0500

    Blank line after a remove-a-conflict action

--- rules.js
+++ rules.js
@@ -10066,6 +10066,7 @@ function do_reflag_space(repair_if_damaged = true) {
 
 	if ((G.action_type === MIL) && has_conflict_marker(G.active_space)) {
 		remove_conflict_marker(G.active_space)
+		log_br() // Leave a blank line
 		return
 	}
 
