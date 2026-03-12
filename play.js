b533ad009de2c1c8fbeb5c67ca773108c17d6889
Brian Reynolds <brian@brianreynolds.net>
2026-03-12 18:26:41 -0400

    hohoho

--- play.js
+++ play.js
@@ -1960,7 +1960,8 @@ function on_update() {
 
 	end_update()
 
-	if (is_bit(SKIPPED_EVENT) && !V.UNDID) {
+	var search_params = new URLSearchParams(window.location.search)
+	if (is_bit(SKIPPED_EVENT) && !V.UNDID && ((R === FRANCE) || (R === BRITAIN)) && (search_params.get("mode") !== "replay")) {
 		if (!skipped_event) { // Prevents this warning from coming up more than once before it has been cleared in between
 			skipped_event = true
 			if (get_preference("eventsies", true)) {
