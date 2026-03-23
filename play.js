b29eaabda280e6d607428631f3d7679d5361ea2c
Tor Andersson <tor@ccxvii.net>
2026-03-23 13:49:38 +0100

    remove duplicate css lines

--- play.js
+++ play.js
@@ -3316,24 +3316,22 @@ function log_awards(codes)
 	awards[REGION_CARIBBEAN] = codes[4] - '0'
 	awards[REGION_INDIA] = codes[5] - '0'
 
-	let wrap = 0
-	let msg = "<div style=\"display: flex; justify-content: center;\">"
+	let msg = []
+	msg.push("<div class=award-table>")
 	for (const region of [ REGION_NORTH_AMERICA, REGION_EUROPE, REGION_CARIBBEAN, REGION_INDIA ]) {
 		var chit = awards[region]
-
-		msg += `<span class="a${chit} award marker black square-sm award-in-log" style="pointer-events: auto;"
-				onmouseenter="_tip_focus_award(${chit}, ${NONE})"
-				onmouseleave="_tip_blur_award()"
-				onmousedown="_tip_click_light('award',${chit})"
-				><span class="region r${region}">${data.regions[region].name}</span></span>`
-		if (++wrap >= 2) {
-			wrap = 0
-			msg += "</div>"
-			msg += "<div style=\"display: flex; justify-content: center;\">"
-		}
+		msg.push(`<div>`)
+		msg.push(`<div class="award-title">${data.regions[region].name}</div>`)
+		msg.push(`<div class="marker square-sm black award a${chit}"
+			onmouseenter="_tip_focus_award(${chit}, ${NONE})"
+			onmouseleave="_tip_blur_award()"
+			onmousedown="_tip_click_light('award',${chit})"
+			></div>`
+		)
+		msg.push(`</div>`)
 	}
-	msg += "</div>"
-	return msg
+	msg.push(`</div>`)
+	return msg.join("")
 }
 
 
@@ -3344,17 +3342,21 @@ function log_demands(codes)
 	global_demand.push(codes[3] - '0')
 	global_demand.push(codes[4] - '0')
 
-	let msg = "<div style=\"display: flex; justify-content: center;\">"
+	let msg = []
+	msg.push(`<div class=demand-table>`)
 	for (var i = 0; i < 3; i++) {
 		var chit = global_demand[i]
-		msg += `<span class="${data.demands[chit].name.toLowerCase()} demand marker square-sm demand-in-log"
-				onmouseenter="_tip_focus_demand('${chit}', 'marker demand small-sm ${data.demands[chit].name.toLowerCase()}')"
-				onmouseleave="_tip_blur_demand()"
-				onmousedown="_tip_click_light('demand',${chit})"
-			></span>`
+		var name = data.demands[chit].name.toLowerCase()
+		console.log("LOG_DEMANDS", chit, name)
+		msg.push(`<div class="marker square-sm demand ${name}"
+			onmouseenter="_tip_focus_demand('${chit}', 'marker square-sm demand ${name}')"
+			onmouseleave="_tip_blur_demand()"
+			onmousedown="_tip_click_light('demand',${chit})"
+			></div>`
+		)
 	}
-	msg += "</div>"
-	return msg
+	msg.push("</div>")
+	return msg.join("")
 }
 
 
